0b9ace3ee217aaa2bb0d4d747b2fa2142c179d4f
Chan-TzuChi <workonly266348@gmail.com>
2025-04-11 20:52:40 +0800

    Update contains_duplicate.py

--- Arrays & Hashing/contains_duplicate.py
+++ Arrays & Hashing/contains_duplicate.py
@@ -34,3 +34,6 @@ if __name__ == "__main__":
     sol = Solution()
     print(sol.hasDuplicate([1, 2, 3, 3]))  # True
     print(sol.hasDuplicate([1, 2, 3, 4]))  # False
+
+    print(sol.hasDuplicate([3, 2, 3, 3, 2, 3]))  # True
+    print(sol.hasDuplicate([1, 2, 3, 4, 5, 6]))  # False
